69cc6768a925f4f0efdf16f80c5a2e1763aa3e8a
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-11-20 12:29:24 +0100

    loader component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 class App extends React.Component {
@@ -16,14 +17,14 @@ class App extends React.Component {
     }
     render() {
         if( this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
+            return <div>Error: {this.state.errorMessage}</div>;
         }
 
         if( !this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />
+            return <SeasonDisplay lat={this.state.lat} />;
         }
 
-        return <div>loading</div>
+        return <Spinner />;
     }
 }
 
