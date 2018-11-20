34a197a683745faee7695a22dbc3adfec9cc5aba
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-11-20 11:52:33 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -10,14 +10,12 @@ class App extends React.Component {
             lat: null,
             errorMessage: ''
         }
+    }
 
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({lat: position.coords.latitude});
-            },
-            err => {
-                this.setState({errorMessage: err.message});
-            }
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
         );
     }
     render() {
