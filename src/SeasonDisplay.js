c4544ceca7179a94ed0d37fbef0cf00e266d744b
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-11-20 10:57:29 +0100

    getting location

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const SeaseonDisplay = () => {
 return <div>SeaseonDisplay</div>
-
 };
 
 export default SeaseonDisplay;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        position => console.log(position),
+        err => console.log(err)
+    );
 return (
         <SeasonDisplay />
-    )
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
