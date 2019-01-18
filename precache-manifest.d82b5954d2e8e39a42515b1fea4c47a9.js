5147f0d279ebfdf9b9b69c0b25d8c2dfc35b5094
Fredrik Lunde <fredrik.lunde3@gmail.com>
2019-01-18 17:05:28 +0100

    Updates

---
+++ precache-manifest.d82b5954d2e8e39a42515b1fea4c47a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "28bfaf534952849d3a82",
+    "url": "/seasons/static/css/main.979d1d2b.chunk.css"
+  },
+  {
+    "revision": "28bfaf534952849d3a82",
+    "url": "/seasons/static/js/main.28bfaf53.chunk.js"
+  },
+  {
+    "revision": "9a83c47ff0c4f16f4b68",
+    "url": "/seasons/static/js/1.9a83c47f.chunk.js"
+  },
+  {
+    "revision": "b162515c83c554475e0d",
+    "url": "/seasons/static/js/runtime~main.b162515c.js"
+  },
+  {
+    "revision": "369172b9df41b35d8e26496ab24fe1c7",
+    "url": "/seasons/index.html"
+  }
+];
